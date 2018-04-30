ad5ea1754df0708ae6350d00a855fc2acd6c049b
Qinxuedi <2424942139@qq.com>
2018-04-30 17:04:58 +0800

    Add files via upload

---
+++ cleared_datasets/beijing_17_18_aq.py
@@ -0,0 +1,81 @@
+leastDict={'miyunshuiku_aq': ['donggaocun_aq', 'dingling_aq', 'yongledian_aq', 'badaling_aq', 'yufa_aq', 'liulihe_aq'], 'tiantan_aq': ['dongsi_aq', 'wanshouxigong_aq', 'nongzhanguan_aq', 'guanyuan_aq', 'aotizhongxin_aq', 'fengtaihuayuan_aq', 'wanliu_aq', 'gucheng_aq', 'zhiwuyuan_aq', 'yungang_aq', 'beibuxinqu_aq'], 'yizhuang_aq': ['daxing_aq', 'tongzhou_aq', 'shunyi_aq', 'fangshan_aq', 'mentougou_aq', 'pingchang_aq', 'huairou_aq', 'miyun_aq', 'pinggu_aq', 'yanqin_aq'], 'pingchang_aq': ['mentougou_aq', 'yanqin_aq', 'huairou_aq', 'shunyi_aq', 'fangshan_aq', 'yizhuang_aq', 'daxing_aq', 'tongzhou_aq', 'miyun_aq', 'pinggu_aq'], 'zhiwuyuan_aq': ['wanliu_aq', 'gucheng_aq', 'beibuxinqu_aq', 'guanyuan_aq', 'fengtaihuayuan_aq', 'yungang_aq', 'wanshouxigong_aq', 'aotizhongxin_aq', 'dongsi_aq', 'tiantan_aq', 'nongzhanguan_aq'], 'qianmen_aq': ['yongdingmennei_aq', 'nansanhuan_aq', 'xizhimenbei_aq', 'dongsihuan_aq'], 'pinggu_aq': ['miyun_aq', 'shunyi_aq', 'huairou_aq', 'tongzhou_aq', 'yizhuang_aq', 'daxing_aq', 'pingchang_aq', 'mentougou_aq', 'fangshan_aq', 'yanqin_aq'], 'beibuxinqu_aq': ['zhiwuyuan_aq', 'wanliu_aq', 'gucheng_aq', 'guanyuan_aq', 'aotizhongxin_aq', 'fengtaihuayuan_aq', 'yungang_aq', 'wanshouxigong_aq', 'dongsi_aq', 'tiantan_aq', 'nongzhanguan_aq'], 'shunyi_aq': ['huairou_aq', 'tongzhou_aq', 'miyun_aq', 'yizhuang_aq', 'pingchang_aq', 'pinggu_aq', 'daxing_aq', 'mentougou_aq', 'fangshan_aq', 'yanqin_aq'], 'tongzhou_aq': ['yizhuang_aq', 'shunyi_aq', 'daxing_aq', 'huairou_aq', 'pinggu_aq', 'miyun_aq', 'pingchang_aq', 'fangshan_aq', 'mentougou_aq', 'yanqin_aq'], 'yungang_aq': ['gucheng_aq', 'fengtaihuayuan_aq', 'zhiwuyuan_aq', 'wanshouxigong_aq', 'wanliu_aq', 'guanyuan_aq', 'beibuxinqu_aq', 'tiantan_aq', 'dongsi_aq', 'aotizhongxin_aq', 'nongzhanguan_aq'], 'yufa_aq': ['liulihe_aq', 'yongledian_aq', 'dingling_aq', 'badaling_aq', 'donggaocun_aq', 'miyunshuiku_aq'], 'wanshouxigong_aq': ['guanyuan_aq', 'tiantan_aq', 'fengtaihuayuan_aq', 'dongsi_aq', 'aotizhongxin_aq', 'nongzhanguan_aq', 'wanliu_aq', 'gucheng_aq', 'zhiwuyuan_aq', 'yungang_aq', 'beibuxinqu_aq'], 'mentougou_aq': ['fangshan_aq', 'pingchang_aq', 'daxing_aq', 'yizhuang_aq', 'yanqin_aq', 'tongzhou_aq', 'shunyi_aq', 'huairou_aq', 'miyun_aq', 'pinggu_aq'], 'dingling_aq': ['badaling_aq', 'miyunshuiku_aq', 'liulihe_aq', 'yufa_aq', 'yongledian_aq', 'donggaocun_aq'], 'donggaocun_aq': ['miyunshuiku_aq', 'yongledian_aq', 'dingling_aq', 'yufa_aq', 'badaling_aq', 'liulihe_aq'], 'liulihe_aq': ['yufa_aq', 'dingling_aq', 'badaling_aq', 'yongledian_aq', 'donggaocun_aq', 'miyunshuiku_aq'], 'xizhimenbei_aq': ['qianmen_aq', 'yongdingmennei_aq', 'nansanhuan_aq', 'dongsihuan_aq'], 'fangshan_aq': ['mentougou_aq', 'daxing_aq', 'yizhuang_aq', 'pingchang_aq', 'tongzhou_aq', 'shunyi_aq', 'yanqin_aq', 'huairou_aq', 'miyun_aq', 'pinggu_aq'], 'nansanhuan_aq': ['yongdingmennei_aq', 'qianmen_aq', 'xizhimenbei_aq', 'dongsihuan_aq'], 'huairou_aq': ['shunyi_aq', 'miyun_aq', 'pingchang_aq', 'tongzhou_aq', 'pinggu_aq', 'yizhuang_aq', 'daxing_aq', 'mentougou_aq', 'yanqin_aq', 'fangshan_aq'], 'badaling_aq': ['dingling_aq', 'liulihe_aq', 'yufa_aq', 'miyunshuiku_aq', 'yongledian_aq', 'donggaocun_aq'], 'dongsi_aq': ['tiantan_aq', 'nongzhanguan_aq', 'aotizhongxin_aq', 'guanyuan_aq', 'wanshouxigong_aq', 'wanliu_aq', 'fengtaihuayuan_aq', 'zhiwuyuan_aq', 'gucheng_aq', 'yungang_aq', 'beibuxinqu_aq'], 'nongzhanguan_aq': ['dongsi_aq', 'tiantan_aq', 'aotizhongxin_aq', 'guanyuan_aq', 'wanshouxigong_aq', 'wanliu_aq', 'fengtaihuayuan_aq', 'zhiwuyuan_aq', 'gucheng_aq', 'beibuxinqu_aq', 'yungang_aq'], 'yanqin_aq': ['pingchang_aq', 'mentougou_aq', 'huairou_aq', 'fangshan_aq', 'shunyi_aq', 'yizhuang_aq', 'daxing_aq', 'miyun_aq', 'tongzhou_aq', 'pinggu_aq'], 'gucheng_aq': ['zhiwuyuan_aq', 'yungang_aq', 'fengtaihuayuan_aq', 'wanliu_aq', 'guanyuan_aq', 'wanshouxigong_aq', 'beibuxinqu_aq', 'aotizhongxin_aq', 'tiantan_aq', 'dongsi_aq', 'nongzhanguan_aq'], 'fengtaihuayuan_aq': ['wanshouxigong_aq', 'guanyuan_aq', 'gucheng_aq', 'wanliu_aq', 'tiantan_aq', 'yungang_aq', 'dongsi_aq', 'zhiwuyuan_aq', 'aotizhongxin_aq', 'nongzhanguan_aq', 'beibuxinqu_aq'], 'wanliu_aq': ['guanyuan_aq', 'zhiwuyuan_aq', 'aotizhongxin_aq', 'fengtaihuayuan_aq', 'gucheng_aq', 'wanshouxigong_aq', 'dongsi_aq', 'beibuxinqu_aq', 'tiantan_aq', 'nongzhanguan_aq', 'yungang_aq'], 'yongledian_aq': ['donggaocun_aq', 'yufa_aq', 'liulihe_aq', 'miyunshuiku_aq', 'dingling_aq', 'badaling_aq'], 'aotizhongxin_aq': ['dongsi_aq', 'nongzhanguan_aq', 'guanyuan_aq', 'tiantan_aq', 'wanliu_aq', 'wanshouxigong_aq', 'fengtaihuayuan_aq', 'zhiwuyuan_aq', 'gucheng_aq', 'beibuxinqu_aq', 'yungang_aq'], 'dongsihuan_aq': ['qianmen_aq', 'yongdingmennei_aq', 'xizhimenbei_aq', 'nansanhuan_aq'], 'daxing_aq': ['yizhuang_aq', 'fangshan_aq', 'tongzhou_aq', 'mentougou_aq', 'shunyi_aq', 'pingchang_aq', 'huairou_aq', 'miyun_aq', 'pinggu_aq', 'yanqin_aq'], 'miyun_aq': ['huairou_aq', 'shunyi_aq', 'pinggu_aq', 'tongzhou_aq', 'pingchang_aq', 'yizhuang_aq', 'daxing_aq', 'mentougou_aq', 'yanqin_aq', 'fangshan_aq'], 'guanyuan_aq': ['wanshouxigong_aq', 'wanliu_aq', 'dongsi_aq', 'aotizhongxin_aq', 'tiantan_aq', 'fengtaihuayuan_aq', 'nongzhanguan_aq', 'zhiwuyuan_aq', 'gucheng_aq', 'yungang_aq', 'beibuxinqu_aq'], 'yongdingmennei_aq': ['qianmen_aq', 'nansanhuan_aq', 'xizhimenbei_aq', 'dongsihuan_aq']}
+
+f=open('/Users/xuedi/course/ml/KDD/datasets/beijing_17_18_aq.csv')
+f2=open('/Users/xuedi/course/ml/KDD/cleared_datasets/beijing_17_18_aq.csv','w')
+cnt=0
+dict={}
+schema=f.readline()
+f2.writelines(schema)
+line=f.readline()
+while line:
+    tuple=line[:-2].split(',')
+    if tuple[0] in dict:
+        dict[tuple[0]].append(tuple)
+    else:
+        dict[tuple[0]]=[tuple]
+    line=f.readline()
+
+# 35*8886
+# for item in dict:
+#     print len(dict[item])
+
+
+def getTimeApproximate(zone,i,j):
+    sum=cnt=0
+    if i>=3 and dict[zone][i-3][j]:
+        sum+=float(dict[zone][i-3][j])
+        cnt+=1
+    if i>=2 and dict[zone][i-2][j]:
+        sum+=float(dict[zone][i-2][j])
+        cnt+=1
+    if i>=1 and dict[zone][i-1][j]:
+        sum+=float(dict[zone][i-1][j])
+        cnt+=1
+    if i+1<8886 and dict[zone][i+1][j]:
+        sum+=float(dict[zone][i+1][j])
+        cnt+=1
+    if i+2<8886 and dict[zone][i+2][j]:
+        sum+=float(dict[zone][i+2][j])
+        cnt+=1
+    if i+3<8886 and dict[zone][i+3][j]:
+        sum+=float(dict[zone][i+3][j])
+        cnt+=1
+    if cnt==0:
+        return 0
+    return sum/cnt
+
+def getGraphicalApproximate(zone,i,j):
+    sum=cnt=0
+    for nearZone in leastDict[zone]:
+        if dict[nearZone][i][j]:
+            sum+=float(dict[nearZone][i][j])
+            cnt+=1
+            if cnt==3:
+                break
+    if cnt==0:
+        return 0
+    return sum/cnt
+
+
+for zone in dict:
+    for i in range(8886):
+        for j in range(8):
+            if dict[zone][i][j]=='':
+                a1=getTimeApproximate(zone,i,j)
+                a2=getGraphicalApproximate(zone,i,j)
+                if a1==0 and a2!=0:
+                    dict[zone][i][j]=str(a2)
+                elif a1!=0 and a2==0:
+                    dict[zone][i][j]=str(a1)
+                else:
+                    dict[zone][i][j]=str((a1+a2)/2)
+        f2.writelines(','.join(dict[zone][i])+'\n')
+
+f.close()
+f2.close()
+
+
+
+
+
+
